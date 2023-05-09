952f5c6b3de5ed539b6fb5335ea34e35e9df200e
Jaime-Marcelo-Sanchez <JMarcelo.SanchezIFW@gmail.com>
2023-05-09 17:52:59 -0500

    xd

--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -3,36 +3,12 @@ package Principal;
 import Paneles.Visualizar;
 
 /*
-INTEGRANTES:
-Liza Guerrero Piero Alexandro	:202114037			
-Loma Aguirre Mariesther Nicole	:202114038			
-Lujan Trujillo Anderson Junior	:202114039			
-Marcelo Sanchez Jaime Mijair	:202114040			
-Zelada Pulido Rodrigo		:202114018
-
-DEBIDO A UN ERROR QUE DESCONOCEMOS, NO DEJA CORRER EL PROGRAMA CON F6 O EL BOTON DE ARRIBA (TRIANGULO VERDE)
-ASI QUE DEBERA HACER CLICK DERECHO>RUN FILE O SHIFT+F6.
-*/ 
-
+@Author: Marcelo Sanchez Jaime
+ */
 public class Main {
-    
-    public static void main(String[] args) {      
+
+    public static void main(String[] args) {
         Visualizar vista = new Visualizar();
         vista.setVisible(true);
     }
 }
-/*
-    Valor del dolar en soles en los ultimos 12 dias hasta hasta hoy 26-02-2023
-    -3.8189
-    -3.7868
-    -3.7896
-    -3.8191
-    -3.8236
-    -3.8353
-    -3.8327
-    -3.8374
-    -3.8292
-    -3.8414
-    -3.8193
-    -3.8413
-*/
